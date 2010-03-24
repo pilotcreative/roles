765453386aba36b6ac35506ada017fe774548deb
Mateusz Drożdżyński <matid@matid.net>
2010-03-24 10:55:55 +0100

    Removed unnecessary code duplication

--- init.rb
+++ init.rb
@@ -1,5 +1,1 @@
-require 'role'
-
-require 'action_filters'
-ActiveRecord::Base.send(:include, ActiveRecord::Aggregations::HasRoles)
-ActionController::Base.send(:extend, Roles::ActionFilters)
+require "roles"
