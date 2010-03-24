2df028f3a553e9c18c4ed272cbc8790f93d02cac
Mateusz Drożdżyński <matid@matid.net>
2010-03-24 10:52:17 +0100

    Do not touch the database on gem load

--- init.rb
+++ init.rb
@@ -1,9 +1,5 @@
 require 'role'
 
-if Role.table_exists?
-  require 'action_filters'
-  ActiveRecord::Base.send(:include, ActiveRecord::Aggregations::HasRoles)
-  ActionController::Base.send(:extend, Roles::ActionFilters)
-else
-  ActiveRecord::Base.class_eval { def self.has_roles; end }
-end
+require 'action_filters'
+ActiveRecord::Base.send(:include, ActiveRecord::Aggregations::HasRoles)
+ActionController::Base.send(:extend, Roles::ActionFilters)
--- lib/active_record/aggregations/has_roles.rb
+++ lib/active_record/aggregations/has_roles.rb
@@ -4,11 +4,13 @@ module ActiveRecord
       def self.included(base)
         base.extend Macro
       end
-      
+
       module Macro
         def has_roles(options = {})
+          return unless Role.table_exists?
+
           write_inheritable_attribute :privileges_table_name, options[:join_table] || "privileges"
-          
+
           has_and_belongs_to_many :roles, :join_table => read_inheritable_attribute(:privileges_table_name), :uniq => true
 
           include InstanceMethods
@@ -16,34 +18,34 @@ module ActiveRecord
 
           Role.all.each do |role|
             define_method("#{role}?") do
-              is?(role.to_s) 
+              is?(role.to_s)
             end
           end
-          
+
           Role.instance_eval <<-TXT
             has_and_belongs_to_many :#{class_name.downcase.pluralize}, :join_table => :#{read_inheritable_attribute(:privileges_table_name)}, :uniq => true
           TXT
         end
       end
-      
+
       module ClassMethods
         def self.extended(base)
           Role.all.each do |role|
             base.named_scope role.to_s.pluralize.to_sym, :include => :roles, :conditions => ["roles.id = ?", role.id]
           end
         end
-        
+
         def roles
           Role.all
         end
-        
+
         def find_with_role(*role_names)
           find(:all, :include => :roles, :conditions => ["roles.name IN (?)", role_names.map(&:to_s)]).uniq
         end
       end
-      
+
       module InstanceMethods
-        # returns true 
+        # returns true
         #  - when user has administrator role
         #  - when user has one of the supplied roles
         def is?(*role_names)
@@ -51,7 +53,7 @@ module ActiveRecord
           return true if roles.include?(Role[:administrator])
           Array(role_names).any? { |name| roles.include?(Role[name]) }
         end
-        
+
         def is_not?(*role_names)
           not is?(*role_names)
         end
